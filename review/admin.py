3be575183731d8f9ba7d8291e595e3769347a7e4
Charles Tack <charles.tack@outlook.com>
2025-01-28 15:43:24 +0000

    add summernote functionality to review

--- review/admin.py
+++ review/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from .models import Review, Comment
-# NB - above when we add additional models we can just add them seperated by a comma
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Review)
+class ReviewAdmin(SummernoteModelAdmin):
+
+    list_display = ('game_title', 'slug', 'status')
+    search_fields = ['game_title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('game_title',)}
+    summernote_fields = ('full_review',)
 
 
 # Register your models here.
-admin.site.register(Review)
 admin.site.register(Comment)
