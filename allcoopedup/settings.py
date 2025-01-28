d79cf740dc34a5f5732d76c13d1e8411e0053248
Charles Tack <charles.tack@outlook.com>
2025-01-28 09:42:30 +0000

    add secret key

--- allcoopedup/settings.py
+++ allcoopedup/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jc=#lj%el&wxby9(5p*9h1z10-fg2)k)d02k!rjfr^b8e^*2-+'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
