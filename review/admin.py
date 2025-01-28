4a7e1d8eed5e1bf014f0d139e3fa9d2e7c0f435c
Charles Tack <charles.tack@outlook.com>
2025-01-28 12:13:09 +0000

    add comment to admin file

--- review/admin.py
+++ review/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Review
+# NB - above when we add additional models we can just add them seperated by a comma
+
 
 # Register your models here.
 admin.site.register(Review)
