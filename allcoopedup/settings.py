61a613c1b499a89a379047590d049b2a382af5d4
Charles Tack <charles.tack@outlook.com>
2025-01-28 09:21:35 +0000

    add database URL to env file

--- allcoopedup/settings.py
+++ allcoopedup/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jc=#lj%el&wxby9(5p*9h1z10-fg2)k)d02k!rjfr^b8e^*2-+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
