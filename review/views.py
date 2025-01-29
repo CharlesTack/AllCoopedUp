ace4735ca06c6d49a81bab424113177e3d856e66
Charles Tack <charles.tack@outlook.com>
2025-01-29 11:30:41 +0000

    add review detail html and customised css

--- review/views.py
+++ review/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import renderm, get_object_or_404
 from django.views import generic
 from .models import Review
 
@@ -8,3 +8,27 @@ class ReviewList(generic.ListView):
     queryset = Review.objects.filter(status=1).order_by("game_title")
     template_name = "review/index.html"
     paginate_by = 6
+
+
+    def review_detail(request, slug):
+    """
+    Display an individual :model:`review.Review`.
+
+    **Context**
+
+    ``review``
+        An instance of :model:`review.Review`.
+
+    **Template:**
+
+    :template:`review/review_detail.html`
+    """
+
+    queryset = Review.objects.filter(status=1)
+    review = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "review/review_detail.html",
+        {"review": review},
+    )
