8d518c7f08900ceb1fe62418ef1068068bbe64f2
Charles Tack <charles.tack@outlook.com>
2025-01-30 09:37:47 +0000

    wired up POST of comment form

--- review/views.py
+++ review/views.py
@@ -26,6 +26,14 @@ def review_detail(request, slug):
     review = get_object_or_404(queryset, slug=slug)
     comments = review.comments.all()
     comment_count = review.comments.filter(approved=True).count()
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.review = review
+            comment.save()
+    
     comment_form = CommentForm()
 
     return render(
