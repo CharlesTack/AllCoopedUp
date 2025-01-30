cc833c2a5d7276133ecdf1e53bffc57a7b0059a1
Charles Tack <charles.tack@outlook.com>
2025-01-30 11:20:33 +0000

    add collaborate POST wiring

--- about/views.py
+++ about/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
-
 # Create your views here.
 
 
@@ -9,6 +9,14 @@ def about_me(request):
     """
     Renders the About page
     """
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I will aim to get back to you within 2-3 working days.'
+            )
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
 
@@ -20,27 +28,3 @@ def about_me(request):
             "collaborate_form": collaborate_form
         },
     )
-
-
-# Commented out below as it's too early for this.
-
-    # if request.method == "POST":
-    #     collaborate_form = CollaborateForm(data=request.POST)
-    #     if collaborate_form.is_valid():
-    #         collaborate_form.save()
-    #         messages.add_message(
-    #             request, messages.SUCCESS,
-    #             'Collaboration request received! I endeavour to respond within 2 working days.'
-    #         )
-    
-    # about = About.objects.all().order_by('-updated_on').first()
-    # collaborate_form = CollaborateForm()
-
-    # return render(
-    #     request,
-    #     "about/about.html",
-    #     {
-    #         "about": about,
-    #         "collaborate_form": collaborate_form
-    #     },
-    # )
