7d2f749e3b65d8abf29a4905a78b125c8ababe55
Charles Tack <charles.tack@outlook.com>
2025-01-31 16:21:45 +0000

    update review/urls.py to include edit and delete review paths

--- review/urls.py
+++ review/urls.py
@@ -5,8 +5,10 @@ urlpatterns = [
     path('', views.ReviewList.as_view(), name='home'),
     path('submit/', views.submit_review, name='submit_review'),
     path('<slug:slug>/', views.review_detail, name='review_detail'),
-    path('<slug:slug>/edit_comment/<int:comment_id>',
+    path('<slug:slug>/edit/', views.edit_review, name='edit_review'),
+    path('<slug:slug>/delete/', views.delete_review, name='delete_review'),
+    path('<slug:slug>/edit_comment/<int:comment_id>/',
          views.comment_edit, name='comment_edit'),
-    path('<slug:slug>/delete_comment/<int:comment_id>',
+    path('<slug:slug>/delete_comment/<int:comment_id>/',
          views.comment_delete, name='comment_delete'),
 ]
