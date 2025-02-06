6b545d0245fada06f3d4708d22ce9655e9c8731c
Charles Tack <charles.tack@outlook.com>
2025-02-06 10:45:31 +0000

    add about docstrings and tidy code

--- about/forms.py
+++ about/forms.py
@@ -4,8 +4,11 @@ from .models import CollaborateRequest
 
 class CollaborateForm(forms.ModelForm):
     """
-    Form class for users to request a collaboration 
+    Form class for users to request a collaboration
     """
     class Meta:
+        """
+        Specify the django model and order of the fields
+        """
         model = CollaborateRequest
         fields = ('name', 'email', 'message')
